8b4a6043c4a02da6fa1c4989c92d83b5c4abb547
Andy Long <mr.a.long@gmail.com>
2022-10-11 16:17:23 -0400

    Round service functions

--- app/Models/Services/RoundService.php
+++ app/Models/Services/RoundService.php
@@ -6,6 +6,9 @@ use App\Models\Fixture;
 use App\Models\Prediction;
 use App\Models\Round;
 use App\Models\RoundStatus;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 
 class RoundService
 {
@@ -17,6 +20,11 @@ class RoundService
     public function __construct(Round $round)
     {
         $this->round = $round;
+
+        if(!$round->id) {
+            $this->round == self::getActiveRound();
+        }
+
     }
 
     /**
@@ -29,7 +37,10 @@ class RoundService
             ->first();
     }
 
-    public function getRoundFixtures()
+    /**
+     * @return Collection
+     */
+    public function getRoundFixtures(): Collection
     {
         return Fixture::where('round_id', $this->round->id)
             ->orderBy('fixture_date', 'ASC')
@@ -37,10 +48,10 @@ class RoundService
     }
 
     /**
-     * @param int $userId
+     * @param User $user
      * @return array
      */
-    public function getUserRoundPredictions(int $userId): array
+    public function getUserRoundPredictions(User $user): array
     {
         $roundFixtures = $this->getRoundFixtures();
 
@@ -48,15 +59,47 @@ class RoundService
 
         foreach ($roundFixtures as $fixture) {
             $predictions[$fixture->id] = Prediction::where([
-                'user_id' => $userId,
+                'user_id' => $user->id,
                 'fixture_id' => $fixture->id
             ])->firstOrCreate([
-                'user_id' => $userId,
+                'user_id' => $user->id,
                 'fixture_id' => $fixture->id
             ]);
         }
 
         return $predictions;
+    }
+
+    /**
+     * @param User $user
+     * @return int
+     */
+    public function userRoundPredictionCount(User $user): int
+    {
+        // Return query result from predictions
+        return DB::table('predictions AS p')
+            // For this user
+            ->where('p.user_id', $user->id)
+            // Join on Fixtures
+            ->leftJoin('fixtures AS f', 'p.fixture_id', '=', 'f.id')
+            // For this round
+            ->where('f.round_id', $this->round->id)
+            // Where there is a prediction made
+            ->whereNotNull('p.predicted_result_id')
+            // Get the count
+            ->count();
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function userRoundSubmitted(User $user): bool
+    {
+        $numRoundFixtures = $this->getRoundFixtures()->count();
+        $numUserRoundPredictions = $this->userRoundPredictionCount($user);
+
+        return $numRoundFixtures === $numUserRoundPredictions;
     }
 
 }
