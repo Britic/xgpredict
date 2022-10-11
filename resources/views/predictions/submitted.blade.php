efc3cc1ef15b628b5e417499ec8d9eaeabb877c2
Andy Long <mr.a.long@gmail.com>
2022-10-11 16:18:13 -0400

    View for completed submission

--- resources/views/predictions/submitted.blade.php
+++ resources/views/predictions/submitted.blade.php
@@ -1,1 +1,14 @@
-<?php
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <div class="row align-content-center">
+            <div class="col-md-6 offset-3">
+                <p class="alert alert-success text-center">Your predictions have been submitted for this round</p>
+            </div>
+
+        </div>
+
+    </div>
+@endsection
